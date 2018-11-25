f9538dcc1658ce7b8873f1faf810fed45df72334
frank <frank.049@hotmail.com>
2018-11-25 13:47:32 +0100

    Finalize helper scripts and template project

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+YEAR="${1}"
+DAY="${2}"
+LEVEL="${3}"
+cd "${YEAR}/day${DAY}"
+cat input.txt | cargo run --release | aoc submit -d "${DAY}" -l "${LEVEL}"
--- template/src/main.rs
+++ template/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{self, Read},
+};
+
+fn get_input() -> io::Result<String> {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf)?;
+    Ok(buf)
+}
+
+fn main() -> Result<(), failure::Error> {
+    let input = get_input()?;
+
+    // solve problem
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[test]
+    fn test_something() {
+        assert_eq!(1, 1)
+    }
 }
