119a53d7b9e4a2689f0cf6923d58fe395c5fece2
frank <frank.049@hotmail.com>
2018-12-02 10:24:56 +0100

    Adapt template

--- 2018/day02/src/main.rs
+++ 2018/day02/src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    io::{self, Read, Write},
+};
 
-const INPUT: &str = include_str!("../input.txt");
-
-#[allow(dead_code)]
-fn part1(s: &str) -> u32 {
+fn level1(s: &str) -> u32 {
     let mut twos = 0;
     let mut threes = 0;
     for l in s.lines() {
@@ -24,54 +24,62 @@ fn part1(s: &str) -> u32 {
     twos * threes
 }
 
-fn part2(s: &str) -> Option<String> {
-    use std::iter;
-
+fn level2(s: &str) -> Option<String> {
     let mut lines = s.lines().collect::<Vec<&str>>();
     lines.sort_unstable();
 
-    for (l1, l2) in lines.iter().flat_map(|l| iter::repeat(l).zip(lines.iter())) {
-        let zipped = l1.chars().zip(l2.chars());
-        if zipped.clone().filter(|(a, b)| a != b).count() == 1 {
-            let substring = zipped.filter(|(a, b)| a == b).map(|(a, _b)| a).collect();
-            return Some(substring);
+    for (i, l1) in lines.iter().enumerate() {
+        for l2 in lines.iter().skip(i) {
+            let zipped = l1.chars().zip(l2.chars());
+            if zipped.clone().filter(|(a, b)| a != b).count() == 1 {
+                let substring = zipped.filter(|(a, b)| a == b).map(|(a, _b)| a).collect();
+                return Some(substring);
+            }
         }
     }
 
     None
 }
 
-fn main() -> Result<(), failure::Error> {
-    // let checksum = part1(INPUT);
-    let commons = part2(INPUT).unwrap();
-    eprintln!("result: {}", commons);
-    print!("{}", commons);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let checksum = level1(&input);
+    writeln!(io::stderr(), "level 1: {}", checksum)?;
+
+    let commons = level2(&input).ok_or_else(|| "failed to find the two correct box IDs")?;
+    writeln!(io::stderr(), "level 2: {}", commons)?;
+
+    // stdout is used to submit solutions
+    writeln!(io::stdout(), "{}", commons)?;
     Ok(())
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    const INPUT: &str = include_str!("../input.txt");
 
     #[test]
-    fn part1_example() {
+    fn level1_example() {
         let input = "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab";
-        assert_eq!(part1(input), 12);
+        assert_eq!(level1(input), 12);
     }
 
     #[test]
-    fn part2_example() {
+    fn level2_example() {
         let input = "abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz";
-        assert_eq!(part2(input), Some("fgij".to_string()));
+        assert_eq!(level2(input), Some("fgij".to_string()));
     }
 
     #[test]
-    fn part1_regression() {
-        assert_eq!(part1(INPUT), 6150);
+    fn level1_regression() {
+        assert_eq!(level1(INPUT), 6150);
     }
 
     #[test]
-    fn part2_regression() {
-        assert_eq!(part2(INPUT), Some("rteotyxzbodglnpkudawhijsc".to_string()));
+    fn level2_regression() {
+        assert_eq!(level2(INPUT), Some("rteotyxzbodglnpkudawhijsc".to_string()));
     }
 }
--- template/src/main.rs
+++ template/src/main.rs
@@ -1,18 +1,50 @@
-// use std::
+use std::io::{self, Read, Write};
 
-const INPUT: &str = include_str!("../input.txt");
+fn level1(s: &str) -> ... {
+    unimplemented!()
+}
 
-fn main() -> Result<(), failure::Error> {
-    // solve problem
+fn level2(s: &str) -> ... {
+    unimplemented!()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let some = level1(&input);
+    writeln!(io::stderr(), "level 1: {}", some)?;
+
+    let thing = level2(&input);
+    writeln!(io::stderr(), "level 2: {}", thing)?;
+
+    // stdout is used to submit solutions
+    writeln!(io::stdout(), "{}", some)?;
     Ok(())
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    const INPUT: &str = include_str!("../input.txt");
 
     #[test]
-    fn test_something() {
+    fn level1_examples() {
         assert_eq!(1, 1)
     }
+
+    #[test]
+    fn level2_examples() {
+        assert_eq!(1, 1)
+    }
+
+    // #[test]
+    // fn level1_regression() {
+    //     assert_eq!(level1(INPUT), 6150);
+    // }
+
+    // #[test]
+    // fn level2_regression() {
+    //     assert_eq!(level2(INPUT), Some("rteotyxzbodglnpkudawhijsc".to_string()));
+    // }
 }
