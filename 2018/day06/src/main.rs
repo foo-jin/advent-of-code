2db689512115b6a184a6dd03ec6f4bc56bf8e7cc
frank <frank.049@hotmail.com>
2018-12-06 12:22:44 +0100

    Clean up logic of 2018 day06

--- 2018/day06/src/main.rs
+++ 2018/day06/src/main.rs
@@ -1,8 +1,10 @@
 use std::{
-    collections::{VecDeque, HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
     io::{self, Read, Write},
     num,
 };
+
+const LIMIT: u32 = 10000;
 
 type Point = (i32, i32);
 
@@ -18,37 +20,8 @@ fn parse_points(s: &str) -> Result<Vec<Point>, num::ParseIntError> {
         .collect()
 }
 
-#[allow(dead_code)]
-fn print_grid(grid: &HashMap<Point, State>) {
-    use itertools::Itertools;
-
-    let mut cells = grid.iter().collect::<Vec<_>>();
-    cells.sort_unstable_by_key(|(p, _s)| p.0);
-    cells.sort_by_key(|(p, _s)| p.1);
-
-    for (_k, group) in &cells.into_iter().group_by(|(p, _s)| p.1) {
-        eprint!("{}", group.map(|c| c.1).join("|"));
-        eprintln!()
-    }
-    eprintln!()
-}
-
-enum State {
-    Claim(Point),
-    Owned(Point),
-    Neutral,
-    Empty,
-}
-use std::fmt;
-
-impl fmt::Display for State {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            State::Claim(p) | State::Owned(p) => write!(f, "{}{}", p.0, p.1),
-            State::Neutral => write!(f, ".."),
-            State::Empty => write!(f, "--"),
-        }
-    }
+fn manhattan(p: Point, q: Point) -> u32 {
+    ((p.0 - q.0).abs() + (p.1 - q.1).abs()) as u32
 }
 
 fn neighbours((px, py): Point) -> impl Iterator<Item = Point> {
@@ -61,65 +34,42 @@ fn neighbours((px, py): Point) -> impl Iterator<Item = Point> {
 }
 
 fn level1(points: &[Point]) -> usize {
-    use maplit::hashset;
-
-    let mut grid = HashMap::new();
-    let mut sizes: HashMap<Point, usize> = HashMap::new();
-    let mut flood = points
-        .iter()
-        .map(|&p| (p, hashset! {p}))
-        .collect::<Vec<(Point, HashSet<Point>)>>();
-    let mut infinite = HashSet::new();
-
     let upper = points.iter().map(|(_x, y)| *y).min().unwrap();
     let lower = points.iter().map(|(_x, y)| *y).max().unwrap();
     let left = points.iter().map(|(x, _y)| *x).min().unwrap();
     let right = points.iter().map(|(x, _y)| *x).max().unwrap();
+    let mut infinite = HashSet::new();
+    let mut counts = HashMap::new();
 
-    while !flood.iter().all(|(_, b)| b.is_empty()) {
-        flood = flood
-            .into_iter()
-            .map(|(p, bound)| {
-                let mut new_bound = HashSet::new();
-                for q in bound {
-                    let e = grid.entry(q).or_insert(State::Empty);
-                    match e {
-                        State::Empty => *e = State::Claim(p),
-                        State::Claim(_) => {*e = State::Neutral; continue },
-                        _ => continue,
-                    }
-                    let (x, y) = q;
-                    if !(x < left || x > right || y < upper || y > lower) {
-                        new_bound.extend(neighbours(q));
-                    } else {
-                        infinite.insert(p);
-                    }
-                }
-                (p, new_bound)
-            })
-            .collect();
+    for y in upper..=lower {
+        for x in left..=right {
+            let p = (x, y);
+            let (closest, dist) = points
+                .iter()
+                .map(|q| (q, manhattan(p, *q)))
+                .min_by_key(|(_q, d)| *d)
+                .unwrap();
 
-        for (_p, state) in grid.iter_mut() {
-            match *state {
-                State::Claim(q) => {
-                    *state = State::Owned(q);
-                    *sizes.entry(q).or_insert(0) += 1;
-                }
-                _ => (),
+            if points
+                .iter()
+                .map(|q| manhattan(p, *q))
+                .filter(|d| *d == dist)
+                .count()
+                > 1
+            {
+                continue;
+            }
+
+            if x == left || x == right || y == upper || y == lower {
+                infinite.insert(closest);
+            } else {
+                *counts.entry(closest).or_insert(0) += 1;
             }
         }
     }
 
-    sizes
-        .iter()
-        .filter(|(p, _v)| !infinite.contains(*p))
-        .map(|(_p, v)| *v)
-        .max()
-        .unwrap()
-}
-
-fn manhattan(p: Point, q: Point) -> u32 {
-    ((p.0 - q.0).abs() + (p.1 - q.1).abs()) as u32
+    counts.retain(|k, _v| !infinite.contains(k));
+    *counts.values().max().unwrap()
 }
 
 fn level2(points: &[Point], limit: u32) -> usize {
@@ -149,7 +99,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let some = level1(&points);
     writeln!(io::stderr(), "level 1: {}", some)?;
 
-    const LIMIT: u32 = 10000;
     let thing = level2(&points, LIMIT);
     writeln!(io::stderr(), "level 2: {}", thing)?;
 
@@ -181,13 +130,15 @@ mod test {
         assert_eq!(level2(&input, 32), 16)
     }
 
-    // #[test]
-    // fn level1_regression() {
-    //     assert_eq!(level1(INPUT), 6150);
-    // }
+    #[test]
+    fn level1_regression() {
+        let input = parse_points(INPUT).unwrap();
+        assert_eq!(level1(&input), 5941);
+    }
 
-    // #[test]
-    // fn level2_regression() {
-    //     assert_eq!(level2(INPUT), Some("rteotyxzbodglnpkudawhijsc".to_string()));
-    // }
+    #[test]
+    fn level2_regression() {
+        let input = parse_points(INPUT).unwrap();
+        assert_eq!(level2(&input, LIMIT), 40244);
+    }
 }
