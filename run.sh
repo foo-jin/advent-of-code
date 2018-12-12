3825afd86f6e465bf9eeb6625d86dc008d7cddfa
frank <frank.049@hotmail.com>
2018-12-12 19:21:48 +0100

    Adjust run script and project template

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ set -e
 YEAR="${1}"
 DAY="${2}"
 LEVEL="${3}"
-# cd "${YEAR}/day${DAY}"
+cd "${YEAR}/day${DAY}"
 cat input.txt | cargo run --release | aoc submit -d "${DAY}" -l "${LEVEL}"
--- template/src/main.rs
+++ template/src/main.rs
@@ -1,22 +1,21 @@
-use std::io::{self, Read, Write};
+use std::{
+    error::Error,
+    io::{self, Read, Write},
+};
 
-fn level1(s: &str) -> ... {
+fn level1(s: &str) -> () {
     unimplemented!()
 }
 
-fn level2(s: &str) -> ... {
-    unimplemented!()
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
     let some = level1(&input);
     writeln!(io::stderr(), "level 1: {}", some)?;
 
-    let thing = level2(&input);
-    writeln!(io::stderr(), "level 2: {}", thing)?;
+    // let thing = level2(&input);
+    // writeln!(io::stderr(), "level 2: {}", thing)?;
 
     // stdout is used to submit solutions
     writeln!(io::stdout(), "{}", some)?;
@@ -32,19 +31,4 @@ mod test {
     fn level1_examples() {
         assert_eq!(1, 1)
     }
-
-    #[test]
-    fn level2_examples() {
-        assert_eq!(1, 1)
-    }
-
-    // #[test]
-    // fn level1_regression() {
-    //     assert_eq!(level1(INPUT), 6150);
-    // }
-
-    // #[test]
-    // fn level2_regression() {
-    //     assert_eq!(level2(INPUT), Some("rteotyxzbodglnpkudawhijsc".to_string()));
-    // }
 }
