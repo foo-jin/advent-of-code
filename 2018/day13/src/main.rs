22fbde62224af00dc466b776c294ac026492a82f
frank <frank.049@hotmail.com>
2018-12-13 12:46:16 +0100

    Refactor 2018 day13 to use vector calculations for location and direction logic

--- 2018/day13/src/main.rs
+++ 2018/day13/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(try_from)]
+
 use std::{
     collections::{HashMap, HashSet},
     error::Error,
@@ -9,32 +11,39 @@ macro_rules! err {
     ($($tt:tt)*) => { Err(Box::<Error>::from(format!($($tt)*))) }
 }
 
-type Point = (usize, usize);
+type Point = [u16; 2];
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
+struct Direction([i8; 2]);
+
+#[allow(non_upper_case_globals)]
+impl Direction {
+    const Up: Direction = Direction([0, -1]);
+    const Down: Direction = Direction([0, 1]);
+    const Left: Direction = Direction([-1, 0]);
+    const Right: Direction = Direction([1, 0]);
+
+    fn lr_turn(&mut self) {
+        let [x, y] = self.0;
+        self.0 = [-y, -x];
+    }
+
+    fn rl_turn(&mut self) {
+        let [x, y] = self.0;
+        self.0 = [y, x];
+    }
 }
 
 impl ops::Add<Turn> for Direction {
     type Output = Self;
 
     fn add(self, turn: Turn) -> Self::Output {
-        use self::Direction::*;
-        match (self, turn) {
+        let dir = match (self.0, turn) {
             (dir, Turn::Straight) => dir,
-            (Up, Turn::Left) => Left,
-            (Up, Turn::Right) => Right,
-            (Down, Turn::Left) => Right,
-            (Down, Turn::Right) => Left,
-            (Left, Turn::Left) => Down,
-            (Left, Turn::Right) => Up,
-            (Right, Turn::Left) => Up,
-            (Right, Turn::Right) => Down,
-        }
+            ([x, y], Turn::Left) => [y, -x],
+            ([x, y], Turn::Right) => [-y, x],
+        };
+        Direction(dir)
     }
 }
 
@@ -70,34 +79,22 @@ impl Cart {
     }
 
     fn update(&mut self) {
-        let (x, y) = self.pos;
-        let new = match self.facing {
-            Direction::Up => (x, y - 1),
-            Direction::Down => (x, y + 1),
-            Direction::Left => (x - 1, y),
-            Direction::Right => (x + 1, y),
-        };
-        self.pos = new;
+        use std::convert::TryFrom;
+        for (x, dx) in self.pos.iter_mut().zip(self.facing.0.iter()) {
+            *x = u16::try_from(i32::from(*x) + i32::from(*dx)).expect("cart went out of bounds");
+        }
     }
 
     fn turn(&mut self, node: Node) {
-        use self::Direction::*;
-        let turn = match node {
+        match node {
             Node::Intersection => {
                 let turn = self.turn[0];
                 self.turn.rotate_left(1);
-                turn
+                self.facing += turn;
             }
-            Node::LRTurn => match self.facing {
-                Up | Down => Turn::Right,
-                Left | Right => Turn::Left,
-            },
-            Node::RLTurn => match self.facing {
-                Up | Down => Turn::Left,
-                Left | Right => Turn::Right,
-            },
-        };
-        self.facing += turn;
+            Node::LRTurn => self.facing.lr_turn(),
+            Node::RLTurn => self.facing.rl_turn(),
+        }
     }
 }
 
@@ -123,6 +120,7 @@ impl str::FromStr for Graph {
         let mut carts = Vec::new();
         for (y, l) in s.lines().enumerate() {
             for (x, c) in l.chars().enumerate() {
+                let pos = [x as u16, y as u16];
                 let id = carts.len();
                 let node = match c {
                     '/' => Node::LRTurn,
@@ -136,13 +134,13 @@ impl str::FromStr for Graph {
                             'v' => Direction::Down,
                             _ => unreachable!(),
                         };
-                        carts.push(Cart::new(id, (x, y), dir));
+                        carts.push(Cart::new(id, pos, dir));
                         continue;
                     }
                     ' ' | '-' | '|' => continue,
                     c => return err!("unexpected character while parsing graph: {}", c),
                 };
-                nodes.insert((x, y), node);
+                nodes.insert(pos, node);
             }
         }
         let positions = carts.iter().map(|c| c.pos).collect();
@@ -159,7 +157,7 @@ impl str::FromStr for Graph {
 impl Graph {
     fn sort_carts(&mut self) {
         self.carts.sort_unstable_by_key(|cart| {
-            let (x, y) = cart.pos;
+            let [x, y] = cart.pos;
             (y, x)
         });
     }
@@ -197,7 +195,7 @@ impl Graph {
     }
 }
 
-fn level1(mut graph: Graph) -> (usize, usize) {
+fn level1(mut graph: Graph) -> Point {
     loop {
         if let Some(collision) = graph.tick() {
             return collision;
@@ -205,7 +203,7 @@ fn level1(mut graph: Graph) -> (usize, usize) {
     }
 }
 
-fn level2(mut graph: Graph) -> (usize, usize) {
+fn level2(mut graph: Graph) -> Point {
     while graph.carts.len() > 1 {
         let _ = graph.tick();
     }
@@ -225,7 +223,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     writeln!(io::stderr(), "level 2: {:?}", thing)?;
 
     // stdout is used to submit solutions
-    write!(io::stdout(), "{},{}", thing.0, thing.1)?;
+    write!(io::stdout(), "{},{}", thing[0], thing[1])?;
 
     Ok(())
 }
@@ -244,7 +242,7 @@ mod test {
     #[test]
     fn level1_examples() {
         let graph = EXAMPLE1.parse().unwrap();
-        assert_eq!(level1(graph), (7, 3))
+        assert_eq!(level1(graph), [7, 3])
     }
 
     const EXAMPLE2: &str = r"/>-<\  
@@ -258,18 +256,18 @@ mod test {
     #[test]
     fn level2_examples() {
         let graph = EXAMPLE2.parse().unwrap();
-        assert_eq!(level2(graph), (6, 4))
+        assert_eq!(level2(graph), [6, 4])
     }
 
     #[test]
     fn level1_regression() {
         let graph = INPUT.parse().unwrap();
-        assert_eq!(level1(graph), (58, 93))
+        assert_eq!(level1(graph), [58, 93])
     }
 
     #[test]
     fn level2_regression() {
         let graph = INPUT.parse().unwrap();
-        assert_eq!(level2(graph), (91, 72))
+        assert_eq!(level2(graph), [91, 72])
     }
 }
