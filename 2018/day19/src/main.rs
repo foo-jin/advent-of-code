512769e72b934f2a8a35dab6ac7e4040e1c3bdbb
frank <frank.049@hotmail.com>
2018-12-19 18:24:14 +0100

    Refactor day 19

--- 2018/day19/src/main.rs
+++ 2018/day19/src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     error::Error,
     io::{self, Read, Write},
+    ops,
     str::FromStr,
 };
 
@@ -14,178 +15,19 @@ macro_rules! format_err {
     ($($tt:tt)*) => { Box::<Error>::from(format!($($tt)*)) }
 }
 
-fn get_next<T>(lines: &mut impl Iterator<Item = T>) -> Result<T, Box<dyn Error>> {
-    lines
-        .next()
-        .ok_or_else(|| format_err!("unexpected end of input"))
-}
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+type Value = u32;
 
-type Reg = usize;
-type Val = usize;
-
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-enum OpCode {
-    Addr(Reg, Reg, Reg),
-    Addi(Reg, Val, Reg),
-    Mulr(Reg, Reg, Reg),
-    Muli(Reg, Val, Reg),
-    Banr(Reg, Reg, Reg),
-    Bani(Reg, Val, Reg),
-    Borr(Reg, Reg, Reg),
-    Bori(Reg, Val, Reg),
-    Setr(Reg, Val, Reg),
-    Seti(Val, Val, Reg),
-    Gtir(Val, Reg, Reg),
-    Gtri(Reg, Val, Reg),
-    Gtrr(Reg, Reg, Reg),
-    Eqir(Val, Reg, Reg),
-    Eqri(Reg, Val, Reg),
-    Eqrr(Reg, Reg, Reg),
-}
-
-impl OpCode {
-    fn get_constructor(ident: &str) -> Result<impl Fn(Val, Val, Val) -> OpCode, Box<dyn Error>> {
-        use self::OpCode::*;
-
-        let f = match ident {
-            "addr" => Addr,
-            "addi" => Addi,
-            "mulr" => Mulr,
-            "muli" => Muli,
-            "banr" => Banr,
-            "bani" => Bani,
-            "borr" => Borr,
-            "bori" => Bori,
-            "setr" => Setr,
-            "seti" => Seti,
-            "gtir" => Gtir,
-            "gtri" => Gtri,
-            "gtrr" => Gtrr,
-            "eqir" => Eqir,
-            "eqri" => Eqri,
-            "eqrr" => Eqrr,
-            _ => return err!("unkown ident"),
-        };
-
-        Ok(f)
-    }
-}
-
-fn parse_opcodes(s: &str) -> Result<Vec<OpCode>, Box<dyn Error>> {
-    fn parse_instruction(l: &str) -> Result<OpCode, Box<dyn Error>> {
-        let mut parts = l.split_whitespace();
-        let ident = get_next(&mut parts)?;
-
-        let mut parts = parts.map(Val::from_str);
-        let a = get_next(&mut parts)??;
-        let b = get_next(&mut parts)??;
-        let c = get_next(&mut parts)??;
-        let instruction = OpCode::get_constructor(ident)?(a, b, c);
-        Ok(instruction)
-    }
-
-    let mut instructions = Vec::new();
-    for l in s.lines() {
-        let instr = parse_instruction(l)?;
-        instructions.push(instr);
-    }
-
-    Ok(instructions)
-}
-
-impl FromStr for Program {
-    type Err = Box<dyn Error>;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let re = regex::Regex::new(r"([-\d]+)")?;
-
-        let mut parts = s.trim().splitn(2, "\n");
-        let first = get_next(&mut parts)?;
-        let caps = re
-            .captures(first)
-            .ok_or_else(|| format_err!("expected a number on line 1"))?;
-        let ip_reg = caps[0].parse()?;
-
-        let program = get_next(&mut parts)?;
-        let instr = parse_opcodes(program)?.into_boxed_slice();
-
-        let result = Program {
-            ip_reg,
-            instr,
-            ..Program::default()
-        };
-
-        Ok(result)
-    }
-}
-
-#[derive(Clone, Debug, Default, Eq, PartialEq)]
-struct Program {
-    ip: usize,
-    ip_reg: Reg,
-    regs: [Val; 6],
-    instr: Box<[OpCode]>,
-}
-
-impl Program {
-    fn step(&mut self) {
-        use self::OpCode::*;
-
-        let regs = &mut self.regs;
-        regs[self.ip_reg] = self.ip as Val;
-        match self.instr[self.ip] {
-            Addr(a, b, c) => regs[c] = regs[a] + regs[b],
-            Addi(a, b, c) => regs[c] = regs[a] + b,
-            Mulr(a, b, c) => regs[c] = regs[a] * regs[b],
-            Muli(a, b, c) => regs[c] = regs[a] * b,
-            Banr(a, b, c) => regs[c] = regs[a] & regs[b],
-            Bani(a, b, c) => regs[c] = regs[a] & b,
-            Borr(a, b, c) => regs[c] = regs[a] | regs[b],
-            Bori(a, b, c) => regs[c] = regs[a] | b,
-            Setr(a, _, c) => regs[c] = regs[a],
-            Seti(a, _, c) => regs[c] = a,
-            Gtir(a, b, c) => regs[c] = if a > regs[b] { 1 } else { 0 },
-            Gtri(a, b, c) => regs[c] = if regs[a] > b { 1 } else { 0 },
-            Gtrr(a, b, c) => regs[c] = if regs[a] > regs[b] { 1 } else { 0 },
-            Eqir(a, b, c) => regs[c] = if a == regs[b] { 1 } else { 0 },
-            Eqri(a, b, c) => regs[c] = if regs[a] == b { 1 } else { 0 },
-            Eqrr(a, b, c) => regs[c] = if regs[a] == regs[b] { 1 } else { 0 },
-        }
-
-        self.ip = regs[self.ip_reg] as usize;
-        self.ip += 1;
-    }
-
-    fn execute_fully(&mut self) {
-        while self.ip < self.instr.len() {
-            self.step();
-        }
-    }
-}
-
-fn level1(mut program: Program) -> Val {
-    program.execute_fully();
-    program.regs[0]
-}
-
-fn level2(mut program: Program) -> Val {
-    program.regs[0] = 1;
-    while program.regs[program.ip_reg] != 1 {
-        program.step();
-    }
-    let seed = *program.regs.iter().max().unwrap();
-    (1..=seed).filter(|k| seed % k == 0).sum()
-}
-
-fn solve() -> Result<(), Box<dyn Error>> {
+fn solve() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
+
     let program = input.parse::<Program>()?;
 
-    let some = level1(program.clone());
+    let some = level1(&program);
     writeln!(io::stderr(), "level 1: {}", some)?;
 
-    let thing = level2(program);
+    let thing = level2(&program);
     writeln!(io::stderr(), "level 2: {}", thing)?;
 
     // stdout is used to submit solutions
@@ -193,7 +35,279 @@ fn solve() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn level1(program: &Program) -> Value {
+    let mut vm = VM::default();
+    vm.exec(program);
+    vm.registers[Register::R0]
+}
+
+fn level2(program: &Program) -> Value {
+    let mut vm = VM::default();
+    vm.registers[Register::R0] = 1;
+    vm.exec(program);
+    vm.registers[Register::R0]
+}
+
+#[derive(Clone, Debug, Default)]
+struct VM {
+    registers: Registers,
+    ip: usize,
+}
+
+impl VM {
+    fn exec(&mut self, prog: &Program) {
+        let ipreg = prog.ipreg;
+        while let Some(instr) = prog.instr.get(self.ip) {
+            if self.ip == 1 {
+                break;
+            }
+            self.registers[ipreg] = self.ip as Value;
+            instr.exec(&mut self.registers);
+            self.ip = self.registers[ipreg] as usize + 1;
+        }
+
+        // short circuit the computation
+        let seed = *self.registers.0.iter().max().unwrap();
+        let result = (1..=seed).filter(|k| seed % k == 0).sum();
+        self.registers[Register::R0] = result;
+    }
+}
+
+struct Program {
+    ipreg: Register,
+    instr: Vec<Instruction>,
+}
+
+impl FromStr for Program {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let mut program = Program {
+            ipreg: Register::R0,
+            instr: vec![],
+        };
+        for line in s.trim().lines() {
+            if line.starts_with("#ip ") {
+                let v = line[4..].parse::<Value>()?;
+                program.ipreg = Register::from_value(v)?;
+            } else {
+                program.instr.push(line.parse()?);
+            }
+        }
+        Ok(program)
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+struct Registers([Value; 6]);
+
+#[derive(Clone, Copy, Debug)]
+enum Register {
+    R0,
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+}
+
+impl Register {
+    fn to_index(&self) -> usize {
+        match self {
+            Register::R0 => 0,
+            Register::R1 => 1,
+            Register::R2 => 2,
+            Register::R3 => 3,
+            Register::R4 => 4,
+            Register::R5 => 5,
+        }
+    }
+
+    fn from_value(v: Value) -> Result<Self> {
+        let r = match v {
+            0 => Register::R0,
+            1 => Register::R1,
+            2 => Register::R2,
+            3 => Register::R3,
+            4 => Register::R4,
+            5 => Register::R5,
+            _ => return err!("invalid register number: {}", v),
+        };
+
+        Ok(r)
+    }
+}
+
+impl ops::Index<Register> for Registers {
+    type Output = Value;
+
+    fn index(&self, r: Register) -> &Value {
+        let i = r.to_index();
+        self.0.index(i)
+    }
+}
+
+impl ops::IndexMut<Register> for Registers {
+    fn index_mut(&mut self, r: Register) -> &mut Value {
+        let i = r.to_index();
+        self.0.index_mut(i)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Instruction {
+    output: Register,
+    op: Opcode,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Opcode {
+    Addr { a: Register, b: Register },
+    Addi { a: Register, b: Value },
+    Mulr { a: Register, b: Register },
+    Muli { a: Register, b: Value },
+    Banr { a: Register, b: Register },
+    Bani { a: Register, b: Value },
+    Borr { a: Register, b: Register },
+    Bori { a: Register, b: Value },
+    Setr { a: Register },
+    Seti { a: Value },
+    Gtir { a: Value, b: Register },
+    Gtri { a: Register, b: Value },
+    Gtrr { a: Register, b: Register },
+    Eqir { a: Value, b: Register },
+    Eqri { a: Register, b: Value },
+    Eqrr { a: Register, b: Register },
+}
+
+impl Instruction {
+    fn exec(&self, regs: &mut Registers) {
+        use self::Opcode::*;
+
+        let result = match self.op {
+            Addr { a, b } => regs[a] + regs[b],
+            Addi { a, b } => regs[a] + b,
+            Mulr { a, b } => regs[a] * regs[b],
+            Muli { a, b } => regs[a] * b,
+            Banr { a, b } => regs[a] & regs[b],
+            Bani { a, b } => regs[a] & b,
+            Borr { a, b } => regs[a] | regs[b],
+            Bori { a, b } => regs[a] | b,
+            Setr { a } => regs[a],
+            Seti { a } => a,
+            Gtir { a, b } => {
+                if a > regs[b] {
+                    1
+                } else {
+                    0
+                }
+            }
+            Gtri { a, b } => {
+                if regs[a] > b {
+                    1
+                } else {
+                    0
+                }
+            }
+            Gtrr { a, b } => {
+                if regs[a] > regs[b] {
+                    1
+                } else {
+                    0
+                }
+            }
+            Eqir { a, b } => {
+                if a == regs[b] {
+                    1
+                } else {
+                    0
+                }
+            }
+            Eqri { a, b } => {
+                if regs[a] == b {
+                    1
+                } else {
+                    0
+                }
+            }
+            Eqrr { a, b } => {
+                if regs[a] == regs[b] {
+                    1
+                } else {
+                    0
+                }
+            }
+        };
+
+        regs[self.output] = result;
+    }
+}
+
+impl FromStr for Instruction {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self> {
+        use self::Opcode::*;
+        use regex::Regex;
+
+        lazy_static::lazy_static! {
+            static ref RE: Regex = Regex::new(
+                r"(?P<opcode>[a-z]+) (?P<a>[0-9]+) (?P<b>[0-9]+) (?P<c>[0-9]+)"
+            ).unwrap();
+        }
+
+        let caps = RE
+            .captures(s)
+            .ok_or_else(|| format_err!("invalid instruction: '{:?}'", s))?;
+        let (a, b, c) = (caps["a"].parse()?, caps["b"].parse()?, caps["c"].parse()?);
+        let mkreg = Register::from_value;
+        let opcode = match &caps["opcode"] {
+            "addr" => Addr {
+                a: mkreg(a)?,
+                b: mkreg(b)?,
+            },
+            "addi" => Addi { a: mkreg(a)?, b },
+            "mulr" => Mulr {
+                a: mkreg(a)?,
+                b: mkreg(b)?,
+            },
+            "muli" => Muli { a: mkreg(a)?, b },
+            "banr" => Banr {
+                a: mkreg(a)?,
+                b: mkreg(b)?,
+            },
+            "bani" => Bani { a: mkreg(a)?, b },
+            "borr" => Borr {
+                a: mkreg(a)?,
+                b: mkreg(b)?,
+            },
+            "bori" => Bori { a: mkreg(a)?, b },
+            "setr" => Setr { a: mkreg(a)? },
+            "seti" => Seti { a },
+            "gtir" => Gtir { a, b: mkreg(b)? },
+            "gtri" => Gtri { a: mkreg(a)?, b },
+            "gtrr" => Gtrr {
+                a: mkreg(a)?,
+                b: mkreg(b)?,
+            },
+            "eqir" => Eqir { a, b: mkreg(b)? },
+            "eqri" => Eqri { a: mkreg(a)?, b },
+            "eqrr" => Eqrr {
+                a: mkreg(a)?,
+                b: mkreg(b)?,
+            },
+            unk => return err!("unknown opcode: {:?}", unk),
+        };
+
+        let instr = Instruction {
+            output: Register::from_value(c)?,
+            op: opcode,
+        };
+        Ok(instr)
+    }
+}
+
+fn main() -> Result<()> {
     if let Err(e) = solve() {
         let stderr = io::stderr();
         let mut w = stderr.lock();
@@ -224,18 +338,18 @@ seti 9 0 5
     #[test]
     fn level1_examples() {
         let program = EX.parse().unwrap();
-        assert_eq!(level1(program), 6);
+        assert_eq!(level1(&program), 6);
     }
 
     #[test]
     fn level1_regression() {
         let program = INPUT.parse().unwrap();
-        assert_eq!(level1(program), 2160);
+        assert_eq!(level1(&program), 2160);
     }
 
     #[test]
     fn level2_regression() {
         let program = INPUT.parse().unwrap();
-        assert_eq!(level2(program), 25945920);
+        assert_eq!(level2(&program), 25945920);
     }
 }
